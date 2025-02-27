4fc26633bc24e2436d6af42cdfa82bc8dc60af16
mubasir umbi <mubasirumbi@gmail.com>
2025-02-28 00:11:04 +0530

    jwt access token sending back to user functionality added

--- apps/backend/src/auth/auth.resolver.ts
+++ apps/backend/src/auth/auth.resolver.ts
@@ -2,17 +2,15 @@ import { Args, Mutation, Resolver } from '@nestjs/graphql';
 import { AuthService } from './auth.service';
 import { SignInInput } from './dto/signin.input';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthPayload } from './entity/auth-payload.entity';
 
 @Resolver()
 export class AuthResolver {
-  constructor(
-    private readonly authService: AuthService,
-  ) {}
+  constructor(private readonly authService: AuthService) {}
 
-  @Mutation(() => String)
+  @Mutation(() => AuthPayload)
   async signIn(@Args('signInInput') signInInput: SignInInput) {
-    
-    const user = await this.authService.validateLocalUser(signInInput)
-    return "successful login"
+    const user = await this.authService.validateLocalUser(signInInput);
+    return await this.authService.login(user);
   }
 }
--- apps/backend/src/auth/auth.service.ts
+++ apps/backend/src/auth/auth.service.ts
@@ -2,10 +2,16 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { SignInInput } from './dto/signin.input';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { verify } from 'argon2';
+import { JwtService } from '@nestjs/jwt';
+import { AuthJwtPayload } from './types/auth-jwtPayload';
+import { User } from '@prisma/client';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly prismaService: PrismaService) {}
+  constructor(
+    private readonly prismaService: PrismaService,
+    private readonly jwtService: JwtService,
+  ) {}
 
   async validateLocalUser({ email, password }: SignInInput) {
     const user = await this.prismaService.user.findUnique({
@@ -20,5 +26,21 @@ export class AuthService {
       throw new UnauthorizedException('Invalid Credintials!');
 
     return user;
+  }
+
+  async generateToken(userId: number) {
+    const payload: AuthJwtPayload = { sub: userId };
+    const accessToken = await this.jwtService.signAsync(payload);
+    return { accessToken };
+  }
+
+  async login(user: User) {
+    const { accessToken } = await this.generateToken(user.id);
+    return {
+      id: user.id,
+      name: user.name,
+      avatar: user.avatar,
+      accessToken,
+    };
   }
 }
