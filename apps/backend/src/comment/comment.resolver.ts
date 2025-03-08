5385061928e41698a95d221ac1ad2d6fa80fe585
mubasir umbi <mubasirumbi@gmail.com>
2025-03-08 23:24:32 +0530

    query for getting all comments by posts completed

--- apps/backend/src/comment/comment.resolver.ts
+++ apps/backend/src/comment/comment.resolver.ts
@@ -3,8 +3,33 @@ import { CommentService } from './comment.service';
 import { CommentEntity } from './entities/comment.entity';
 import { CreateCommentInput } from './dto/create-comment.input';
 import { UpdateCommentInput } from './dto/update-comment.input';
+import { DEFAULT_PAGE_SIZE } from 'src/constants';
 
 @Resolver(() => CommentEntity)
 export class CommentResolver {
   constructor(private readonly commentService: CommentService) {}
+
+  @Query(() => [CommentEntity])
+  getPostComment(
+    @Args('postId', { type: () => Int! }) postId: number,
+    @Args('take', {
+      type: () => Int!,
+      nullable: true,
+      defaultValue: DEFAULT_PAGE_SIZE,
+    })
+    take: number,
+    @Args('skip', {
+      type: () => Int!,
+      nullable: true,
+      defaultValue: 0,
+    })
+    skip: number,
+  ) {
+    return this.commentService.findManyByPost({ postId, take, skip });
+  }
+
+  @Query(() => Int)
+  async postCommentCount(@Args('postId', { type: () => Int! }) postId: number) {
+    return this.commentService.count(postId);
+  }
 }
